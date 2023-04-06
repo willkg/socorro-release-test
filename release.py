4fdeb747e16c669eed86f40dba83a96da6c6a7bf
Will Kahn-Greene <willkg@mozilla.com>
2023-04-06 13:26:10 -0400

    Fix release.py tagging

--- release.py
+++ release.py
@@ -362,12 +362,17 @@ def run():
 
     # If there's already a tag, then increment the -N until we find a tag name
     # that doesn't exist, yet
+    print(f"{tag_name = }")
     existing_tags = check_output(f'git tag -l "{tag_name}*"').splitlines()
+    print(f"{existing_tags = }")
     if existing_tags:
+        tag_name_attempt = tag_name
         index = 2
-        while tag_name not in existing_tags:
+        while tag_name_attempt in existing_tags:
+            print(f"{tag_name_attempt = }")
+            tag_name_attempt = f"{tag_name}-{index}"
             index += 1
-            tag_name = f"{tag_name}-{index}"
+        tag_name = tag_name_attempt
 
     if args.cmd == "make-bug":
         make_bug(
